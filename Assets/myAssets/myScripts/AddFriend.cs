4847a472f32e9a74d9a6ec683b07241b7d9f2cf3
langwenhui <langwenhui86@gmail.com>
2022-07-04 23:36:37 -0500

    add friend (toy version)

---
+++ Assets/myAssets/myScripts/AddFriend.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddFriend : MonoBehaviour
+{
+    public void AddNewFriend(GameObject friendToCopy)
+    {
+      GameObject newFriend = GameObject.Instantiate(friendToCopy);
+      newFriend.transform.parent = friendToCopy.transform.parent; 
+    }
+}
