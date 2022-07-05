e2d3db65615384fff61332c36dd37c1e4120ee59
langwenhui <langwenhui86@gmail.com>
2022-07-04 23:25:47 -0500

    added delete function

---
+++ Assets/myAssets/myScripts/DeleteFriend.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteFriend : MonoBehaviour
+{
+    public void DestroyGameObject(GameObject objectToDestroy)
+    {
+      Destroy (objectToDestroy);
+    }
+}
