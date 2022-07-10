9e0f0d5031503b8d2b4b239ccac8df313e153212
vinlincc <105663735+vinlincc@users.noreply.github.com>
2022-07-10 11:20:22 -0400

    Delete Friend Window

--- Assets/myAssets/myScripts/FriendlistController.cs
+++ Assets/myAssets/myScripts/FriendlistController.cs
@@ -12,6 +12,11 @@ public class FriendlistController : MonoBehaviour
     [SerializeField] GameObject friendlistBtnPref;
     [SerializeField] Transform friendlistBtnParent;
     [SerializeField] Image friendProfileImage;
+
+    [SerializeField] TextMeshProUGUI friendNameDeleteText;
+    [SerializeField] Image friendProfileDeleteImage;
+    [SerializeField] GameObject friendlistDeleteWindow;
+
     public TextAsset jsonFile;
     private Dictionary<int, string> friendsName = new Dictionary<int, string>();
     private Dictionary<int, int> friendsPicId = new Dictionary<int, int>();
@@ -30,8 +35,12 @@ public class FriendlistController : MonoBehaviour
             GameObject friendlistBtnObj = Instantiate(friendlistBtnPref, friendlistBtnParent) as GameObject;
             friendlistBtnObj.GetComponent<FriendlistButtonItem>().friendIndex = friend.uid;
             friendlistBtnObj.GetComponent<FriendlistButtonItem>().friendName = friend.friendName;
-            //friendlistBtnObj.GetComponent<FriendlistButtonItem>().friendPicId = friend.picid;
             friendlistBtnObj.GetComponent<FriendlistButtonItem>().friendlistController = this;
+
+
+            friendlistBtnObj.GetComponentInChildren<FriendlistDeleteButton>().friendIndex = friend.uid;
+            friendlistBtnObj.GetComponentInChildren<FriendlistDeleteButton>().friendlistController = this;
+
             friendsName[friend.uid] = friend.friendName;
             friendsPicId[friend.uid] = friend.picid;
                  
@@ -42,8 +51,15 @@ public class FriendlistController : MonoBehaviour
     {
         friendNameText.text = friendsName[friendIndex];
         friendIndexText.text = "uid: " + (friendIndex);
-        //friendProfileImage.sprite = sprites[friendsPicId[friendIndex]];
         friendProfileImage.sprite = Resources.Load<Sprite>(string.Format("PenguinProfileSamples/profile{0}", friendsPicId[friendIndex]));
         friendProfileImage.SetNativeSize();
+    }
+
+    public void OnFriendlistDeleteButtonClick(int friendIndex)
+    {
+        friendlistDeleteWindow.SetActive(true);
+        friendNameDeleteText.text = string.Format("Are you sure to delete\n {0}?", friendsName[friendIndex]);
+        friendProfileDeleteImage.sprite = Resources.Load<Sprite>(string.Format("PenguinProfileSamples/profile{0}", friendsPicId[friendIndex]));
+        friendProfileDeleteImage.SetNativeSize();
     }
 }
