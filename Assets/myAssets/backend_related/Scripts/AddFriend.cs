d6cd69821b4b343b1359f9c963d201665a299736
lousypoetry <88774795+lousypoetry@users.noreply.github.com>
2022-07-13 09:43:09 +0800

    test error

--- Assets/myAssets/backend_related/Scripts/AddFriend.cs
+++ Assets/myAssets/backend_related/Scripts/AddFriend.cs
@@ -15,21 +15,23 @@ public class AddFriend : MonoBehaviour
     private MySqlAccess mysql;
 
     // required input data for this script  
-    public InputField myUid;
+    public string myUid; 
     public InputField friendUid;
     
     private void Start()
     {
+        myUid = GameObject.FindGameObjectWithTag("RegistrationTag").GetComponent<Login>().uid.ToString();
         mysql = new MySqlAccess(host, port, userName, password, databaseName);
+        Debug.Log("Current scene: FriendList. Current userid: " + myUid.ToString());
     }
 
     // Update is called once per frame
     public void AddFriendClick()
     {
-        if (myUid.text != null && friendUid.text != null)
+        if (myUid != "-1" && friendUid.text != null)
         {
             mysql.OpenSql();
-            DataSet queryResult = mysql.Select("friendinfo", new string[] { "userid2" }, new string[] {"`" + "userid1" + "`", "`" + "userid2" + "`"}, new string[] { "=", "=" }, new string[] { myUid.text, friendUid.text});
+            DataSet queryResult = mysql.Select("friendinfo", new string[] { "userid2" }, new string[] {"`" + "userid1" + "`", "`" + "userid2" + "`"}, new string[] { "=", "=" }, new string[] { myUid, friendUid.text});
             if (queryResult!= null)
             {
                 DataTable table = queryResult.Tables[0];
@@ -40,12 +42,12 @@ public class AddFriend : MonoBehaviour
                 }
                 else
                 {
-                    string query1 = mysql.Insert("friendinfo", new string[] { "userid1", "userid2" }, new string[] { myUid.text, friendUid.text });
+                    string query1 = mysql.Insert("friendinfo", new string[] { "userid1", "userid2" }, new string[] { myUid, friendUid.text });
                     MySqlCommand cmd1 = new MySqlCommand(query1, mysql.mySqlConnection);
                     MySqlDataReader dataReader1 = cmd1.ExecuteReader();
                     dataReader1.Close();
                     
-                    string query2 = mysql.Insert("friendinfo", new string[] { "userid1", "userid2" }, new string[] { friendUid.text, myUid.text });
+                    string query2 = mysql.Insert("friendinfo", new string[] { "userid1", "userid2" }, new string[] { friendUid.text, myUid });
                     MySqlCommand cmd2 = new MySqlCommand(query2, mysql.mySqlConnection);
                     MySqlDataReader dataReader2 = cmd2.ExecuteReader();
                     dataReader2.Close();
--- Assets/myAssets/backend_related/Scripts/Login.cs
+++ Assets/myAssets/backend_related/Scripts/Login.cs
@@ -14,7 +14,8 @@ public class Login : MonoBehaviour
      // required input data for this script
     public InputField loginname;
     public InputField loginpassword;
-   
+    public int uid = -1;
+
     private void Start()
     {
         mysql = new MySqlAccess(host, port, userName, password, databaseName);
@@ -22,18 +23,20 @@ public class Login : MonoBehaviour
     public void LoginClick()
     {
         mysql.OpenSql();
-        DataSet queryResult = mysql.Select("userinfo", new string[] { "username" }, new string[] { "`" + "username" + "`", "`" + "password" + "`" }, new string[] { "=", "=" }, new string[] { loginname.text,loginpassword.text });
+        DataSet queryResult = mysql.Select("userinfo", new string[] { "userid" }, new string[] { "`" + "username" + "`", "`" + "password" + "`" }, new string[] { "=", "=" }, new string[] { loginname.text,loginpassword.text });
         if (queryResult != null)
         {
             DataTable table = queryResult.Tables[0];
             if (table.Rows.Count > 0)
             {
-                Debug.Log("Login successfully." + " Username: " + table.Rows[0][0]);
+                // if successfully login, store the current uid
+                uid = (int)table.Rows[0][0];
+                Debug.Log("Current scene: RegistrationTest. Login successfully." + " Userid: " + table.Rows[0][0]);
                 mysql.CloseSql();
                 return;
             }
         }
-        Debug.Log("Login failed.");
+        Debug.Log("Current scene: RegistrationTest. Login failed.");
         mysql.CloseSql();
     }
 }
