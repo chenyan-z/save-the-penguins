07ecba1a29a5adaa3b59d0e1941be8b874535b79
langwenhui <langwenhui86@gmail.com>
2022-07-14 12:13:14 -0500

    friend request connected to backend

--- Assets/myAssets/backend_related/Scripts/CheckFriendRequests.cs
+++ Assets/myAssets/backend_related/Scripts/CheckFriendRequests.cs
@@ -22,16 +22,16 @@ using UnityEngine.SceneManagement;
 public class CheckFriendRequests : MonoBehaviour
 {
     private string host = "120.77.148.135";
-    private string port = "3306";     
+    private string port = "3306";
     private string userName = "root";
     private string password = "penguinspy123456";
     private string databaseName = "penguintest";
     private MySqlAccess mysql;
 
-    // required input data for this script  
-    public string myUid; 
+    // required input data for this script
+    public string myUid;
     public Friend friendRequest;
-    
+
     private void Start()
     {
         myUid = GameObject.FindGameObjectWithTag("RegistrationTag").GetComponent<Login>().uid.ToString();
@@ -48,10 +48,10 @@ public class CheckFriendRequests : MonoBehaviour
             if (queryResult != null)
             {
                 DataTable table = queryResult.Tables[0];
-                if (table.Rows.Count > 0 && (int)table.Rows[0][0] != 0)        
+                if (table.Rows.Count > 0 && (int)table.Rows[0][0] != 0)
                 {
                     DataSet queryFriend = mysql.Select("userinfo", new string[] { "username", "picid" }, new string[] {"`" + "userid" + "`"}, new string[] { "=" }, new string[] { table.Rows[0][0].ToString() });
-                    if (queryFriend != null) 
+                    if (queryFriend != null)
                     {
                         //uid
                         friendRequest.uid = (int)table.Rows[0][0];
@@ -68,6 +68,7 @@ public class CheckFriendRequests : MonoBehaviour
                 else
                 {
                     Debug.Log("No friend requests.");
+                    Debug.Log((friendRequest.uid).ToString());
                 }
             }
             mysql.CloseSql();
@@ -78,7 +79,7 @@ public class CheckFriendRequests : MonoBehaviour
         }
         Scene scene = SceneManager.GetActiveScene();
         if (scene.name == "FriendList")
-        {   
+        {
             SceneManager.LoadScene("FriendRequest");
         }
         return;
--- Assets/myAssets/myScripts/FriendRequestController.cs
+++ Assets/myAssets/myScripts/FriendRequestController.cs
@@ -6,10 +6,11 @@ using TMPro;
 
 public class FriendRequestController : MonoBehaviour
 {
-    private string[] tmpstr = {"Guoba", "Guobaba", "Guobababa"};
+    private string[] tmpstr = {"Guoba"}; //this is temporary.
     [SerializeField] TextMeshProUGUI requestName;
     [SerializeField] GameObject requestPref;
     [SerializeField] Transform requestParent;
+    private CheckFriendRequests checkFrinedRequests;
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +21,29 @@ public class FriendRequestController : MonoBehaviour
 
     private void LoadRequest()
     {
-        foreach (string tmpname in tmpstr)
-        {
-            GameObject requestObj = Instantiate(requestPref, requestParent) as GameObject;
-            requestObj.GetComponent<RequestItem>().requestFriendName = tmpname;
-            //galleryImgObj.GetComponent<GalleryItem>().galleryPenguinName = penguin.penguinName;
-            //galleryImgObj.GetComponent<GalleryItem>().galleryPenguinPicid = penguin.penguinPicid;
-            //friendlistBtnObj.GetComponent<FriendlistButtonItem>().friendPicId = friend.picid;
-            //friendlistBtnObj.GetComponent<FriendlistButtonItem>().friendlistController = this;
-            //friendsName[friend.uid] = friend.friendName;
-            //friendsPicId[friend.uid] = friend.picid;
+        checkFrinedRequests = GameObject.FindGameObjectWithTag("RequestTag").GetComponent<CheckFriendRequests>();
+        checkFrinedRequests.CheckFriendRequestsClick();
+
+        //for now only 1 friend request is returend.
+        Friend requestFriend = checkFrinedRequests.friendRequest;
+
+
+        if(requestFriend.uid != 0){
+          //this is temporary.
+          tmpstr[0] = requestFriend.friendName;
+
+
+          foreach (string tmpname in tmpstr)
+          {
+              GameObject requestObj = Instantiate(requestPref, requestParent) as GameObject;
+              requestObj.GetComponent<RequestItem>().requestFriendName = tmpname;
+              //galleryImgObj.GetComponent<GalleryItem>().galleryPenguinName = penguin.penguinName;
+              //galleryImgObj.GetComponent<GalleryItem>().galleryPenguinPicid = penguin.penguinPicid;
+              //friendlistBtnObj.GetComponent<FriendlistButtonItem>().friendPicId = friend.picid;
+              //friendlistBtnObj.GetComponent<FriendlistButtonItem>().friendlistController = this;
+              //friendsName[friend.uid] = friend.friendName;
+              //friendsPicId[friend.uid] = friend.picid;
+          }
         }
     }
 }
