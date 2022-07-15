0a76330f41d587c18e9392adf5e2f44786fdcf00
Chenyan Zhang <chenyan_zhang@berkeley.edu>
2022-07-14 23:36:15 -0400

    collect scissors from some tool hint in scene CollectScissors

--- Assets/myAssets/myScripts/ObjectSpawner.cs
+++ Assets/myAssets/myScripts/ObjectSpawner.cs
@@ -6,25 +6,80 @@ using UnityEngine.XR.ARSubsystems;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    public GameObject objectToSpawn;
+    [SerializeField]
+    ARRaycastManager rayManager;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    [SerializeField]
+    GameObject hintToSpawn;
+    GameObject spawnedHint;
+    bool hinted;
+
+    [SerializeField]
+    GameObject objectToSpawn;
+    GameObject spawnedObject;
+
+    Camera arCam;
     private PlacementIndicator placementIndicator;
-    private ARPlaneManager planeManager;
 
     void Start ()
     {
+        hinted = false;
+        spawnedHint = null;
+        spawnedObject = null;
+        arCam = GameObject.Find("AR Camera").GetComponent<Camera>();
         placementIndicator = FindObjectOfType<PlacementIndicator>();
-        planeManager = FindObjectOfType<ARPlaneManager>();
+        // rayManager = FindObjectOfType<ARRaycastManager>();
     }
 
     void Update ()
     {
-        // if(planeManager.enabled)
-        // {
-            if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
-            {   
-                GameObject obj = Instantiate(objectToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
-            }
-        // }
+        if (Input.touchCount == 0)
+            return;
         
+        // on touch:
+
+        if (!hinted && Input.GetTouch(0).phase == TouchPhase.Began){ // if not hinted yet, generate hint
+            UnityEngine.Debug.Log("try to place hint");
+            hinted = true;
+            spawnedHint = Instantiate(hintToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            return;
+        }
+        
+        RaycastHit hit;
+        Ray ray = arCam.ScreenPointToRay(Input.GetTouch(0).position);
+        if (rayManager.Raycast(Input.GetTouch(0).position, hits))
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began && spawnedObject == null)
+            {
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject.tag == "Scissors")
+                    {
+                        spawnedObject = hit.collider.gameObject;
+                    }
+                    else
+                    {
+                        UnityEngine.Debug.Log("try to destroy hint and drag object");
+                        Destroy(spawnedHint); // destroy hint before spawning tool
+                        spawnedHint = null;
+                        spawnedObject = Instantiate(objectToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+                        UnityEngine.Debug.Log("object spawned");
+                    }
+                }
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Moved && spawnedObject != null)
+            {
+                spawnedObject.transform.position = hits[0].pose.position;
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Ended){
+                Destroy(spawnedObject);
+                spawnedObject = null;
+                
+                // TODO: object collected!
+            }
+        }
     }
 }
+
--- Assets/myAssets/myScripts/PlacementIndicator.cs
+++ Assets/myAssets/myScripts/PlacementIndicator.cs
@@ -6,109 +6,46 @@ using UnityEngine.XR.ARSubsystems;
 
 public class PlacementIndicator : MonoBehaviour
 {
-    private ARPlaneManager planeManager;
-    private ARRaycastManager rayManager;
-    private GameObject visual;
-    
-    //used for manipulate existing ar objects
-    // private PlacementObjects[] placedObjects;
-    // private PlacementObjects lastSelectedObject;
-    // private Vector2 touchPosition = default;
+    [SerializeField]
+    ARRaycastManager rayManager;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
-    // [SerializeField]
-    // private Camera arCamera;
+    private ARPlaneManager planeManager;
+
+    private GameObject visual;
+
     void Start ()
     {
         // get the components
-        rayManager = FindObjectOfType<ARRaycastManager>();
-        planeManager = FindObjectOfType<ARPlaneManager>();
-        // detectoinTriggle = FindObjectOfType<PlaneDetectionTriggle>();
         visual = transform.GetChild(0).gameObject;
+        planeManager = FindObjectOfType<ARPlaneManager>();
 
         // hide the placement visual
         visual.SetActive(false);
     }
 
     void Update ()
-    {   
+    {
         if(planeManager.enabled) // clue-detection mode
         {
             // shoot a raycast from the center of the screen
-            List<ARRaycastHit> hits = new List<ARRaycastHit>();
-            rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.PlaneWithinBounds);
-            // if we hit an AR plane, update the position and rotation
-            if (hits.Count > 0)
-            {
-                transform.position = hits[0].pose.position;
-                transform.rotation = hits[0].pose.rotation;
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
 
-                if (!visual.activeInHierarchy)
-                {
-                    visual.SetActive(true);
-                }
+        // if we hit an AR plane, update the position and rotation
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if (!visual.activeInHierarchy)
+            {
+                visual.SetActive(true);
             }
+        }
         }
         else // object-interaction mode
         {
             visual.SetActive(false);
         }
     }
-
-
-    // void Update ()
-    // {
-    //     // shoot a raycast from the center of the screen
-    //     List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    //     rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.PlaneWithinBounds);
-
-    //     // if we hit an AR plane, update the position and rotation
-    //     if (hits.Count > 0)
-    //     {
-    //         if(planeManager.enabled)
-    //         {
-    //             // UnityEngine.Debug.Log("planeManager enable");
-    //             transform.position = hits[0].pose.position;
-    //             transform.rotation = hits[0].pose.rotation;
-
-    //             if (!visual.activeInHierarchy)
-    //             {
-    //                 // UnityEngine.Debug.Log("set visual active");
-    //                 visual.SetActive(true);
-    //             }
-
-    //             //if the user touch the screen
-    //             // if (Input.touchCount > 0)
-    //             // {
-    //             //     Touch touch = Input.GetTouch(0);
-    //             //     touchPosition = touch.position;
-    //             //     if(touch.phase == TouchPhase.Began)
-    //             //     {
-    //             //         Ray ray = arCamera.ScreenPointToRay(touch.position);
-    //             //         RaycastHit hitObject;
-    //             //         if(Physics.Raycast(ray, out hitObject))
-    //             //         {
-    //             //             lastSelectedObject = hitObject.transform.GetComponent<PlacementObjects>();
-    //             //             if(lastSelectedObject != null)
-    //             //             {
-    //             //                 PlacementObjects[] allOtherObjects = FindObjectsOfType<PlacementObjects>();
-    //             //                 foreach(PlacementObjects placementObjects in allOtherObjects)
-    //             //                 {
-    //             //                     placementObjects.Selected = placementObjects == lastSelectedObject;
-    //             //                 }
-    //             //             }
-    //             //         }
-    //             //     } 
-    //             //     if(touch.phase == TouchPhase.Ended)
-    //             //     {
-    //             //         lastSelectedObject.Selected = false;
-    //             //     }
-    //             // }
-    //         }
-    //         else
-    //         {
-    //             // UnityEngine.Debug.Log("Set cursor disable");
-    //             visual.SetActive(false);
-    //         }
-    //     }
-    // }
 }
