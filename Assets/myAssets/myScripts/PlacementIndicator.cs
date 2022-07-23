f96ba9c26bc7184b22fcb4eed2020e3b53422af6
kexin-Star <89749795+kexin-Star@users.noreply.github.com>
2022-07-22 21:55:55 -0400

    6 modified file while running and building the project
    I have no choice but first commit them to the branch before I pull the new commits.

--- Assets/myAssets/myScripts/PlacementIndicator.cs
+++ Assets/myAssets/myScripts/PlacementIndicator.cs
@@ -10,22 +10,13 @@ public class PlacementIndicator : MonoBehaviour
     private ARRaycastManager rayManager;
     private GameObject visual;
     
-    [SerializeField]
-    ARRaycastManager raycastManager;
-    List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    //used for manipulate existing ar objects
-    // private PlacementObjects[] placedObjects;
-    // private PlacementObjects lastSelectedObject;
-    // private Vector2 touchPosition = default;
-
     void Start ()
     {
         // get the components
-        // rayManager = FindObjectOfType<ARRaycastManager>();
+        rayManager = FindObjectOfType<ARRaycastManager>();
         planeManager = FindObjectOfType<ARPlaneManager>();
         // detectoinTriggle = FindObjectOfType<PlaneDetectionTriggle>();
         visual = transform.GetChild(0).gameObject;
-        planeManager = FindObjectOfType<ARPlaneManager>();
 
         // hide the placement visual
         visual.SetActive(false);
@@ -33,22 +24,28 @@ public class PlacementIndicator : MonoBehaviour
 
     void Update ()
     {
-        if(planeManager.enabled) // clue-detection mode
+        // shoot a raycast from the center of the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.PlaneWithinBounds);
+
+        // if we hit an AR plane, update the position and rotation
+        if (hits.Count > 0)
         {
-            // shoot a raycast from the center of the screen
-            raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.PlaneWithinBounds);
-            // if we hit an AR plane, update the position and rotation
-            if (hits.Count > 0)
+            if(planeManager.enabled)
             {
+                // UnityEngine.Debug.Log("planeManager enable");
                 transform.position = hits[0].pose.position;
                 transform.rotation = hits[0].pose.rotation;
+
                 if (!visual.activeInHierarchy)
                 {
+                    // UnityEngine.Debug.Log("set visual active");
                     visual.SetActive(true);
                 }
             }
-            else // object-interaction mode
+            else
             {
+                // UnityEngine.Debug.Log("Set cursor disable");
                 visual.SetActive(false);
             }
         }
